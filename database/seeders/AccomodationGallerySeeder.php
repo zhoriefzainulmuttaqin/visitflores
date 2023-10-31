f6405bca98063f786543411a78708f97d890e71d
hisyamabilkhoir <hisamabilkhoir99@gmail.com>
2023-10-31 20:22:03 +0700

    fix seeder hotels

--- database/seeders/AccomodationGallerySeeder.php
+++ database/seeders/AccomodationGallerySeeder.php
@@ -16,39 +16,93 @@ class AccomodationGallerySeeder extends Seeder
         $galleries = ([
             ([
                 "data_id"   => 1,
-                "name"   => "Foto Depan",
-                "picture"   => "bg.png",
-                "type"   => 1,
-            ]),
-            ([
-                "data_id"   => 2,
-                "name"   => "Foto Depan",
-                "picture"   => "bg.png",
-                "type"   => 1,
-            ]),
-            ([
-                "data_id"   => 3,
-                "name"   => "Foto Depan",
-                "picture"   => "bg.png",
-                "type"   => 1,
+                "name"   => "Galeri hotel aston",
+                "picture"   => "galeri-aston-1.webp",
             ]),
             ([
                 "data_id"   => 1,
-                "name"   => "Foto Depan",
-                "picture"   => "hotel.png",
-                "type"   => 2,
+                "name"   => "Galeri hotel aston",
+                "picture"   => "galeri-aston-2.webp",
+            ]),
+            ([
+                "data_id"   => 1,
+                "name"   => "Galeri hotel aston",
+                "picture"   => "galeri-aston-3.webp",
             ]),
             ([
                 "data_id"   => 2,
-                "name"   => "Foto Depan",
-                "picture"   => "hotel.png",
-                "type"   => 2,
+                "name"   => "Galeri hotel verse",
+                "picture"   => "galeri-verse-1.webp",
+            ]),
+            ([
+                "data_id"   => 2,
+                "name"   => "Galeri hotel verse",
+                "picture"   => "galeri-verse-2.webp",
+            ]),
+            ([
+                "data_id"   => 2,
+                "name"   => "Galeri hotel verse",
+                "picture"   => "galeri-verse-3.webp",
             ]),
             ([
                 "data_id"   => 3,
-                "name"   => "Foto Depan",
-                "picture"   => "hotel.png",
-                "type"   => 2,
+                "name"   => "Galeri hotel batiqa",
+                "picture"   => "galeri-batiqa-1.webp",
+            ]),
+            ([
+                "data_id"   => 3,
+                "name"   => "Galeri hotel batiqa",
+                "picture"   => "galeri-batiqa-2.webp",
+            ]),
+            ([
+                "data_id"   => 3,
+                "name"   => "Galeri hotel batiqa",
+                "picture"   => "galeri-batiqa-3.webp",
+            ]),
+            ([
+                "data_id"   => 4,
+                "name"   => "Galeri hotel cordela",
+                "picture"   => "galeri-cordela-1.webp",
+            ]),
+            ([
+                "data_id"   => 4,
+                "name"   => "Galeri hotel cordela",
+                "picture"   => "galeri-cordela-2.webp",
+            ]),
+            ([
+                "data_id"   => 4,
+                "name"   => "Galeri hotel cordela",
+                "picture"   => "galeri-cordela-3.webp",
+            ]),
+            ([
+                "data_id"   => 5,
+                "name"   => "Galeri hotel swiss",
+                "picture"   => "galeri-swiss-1.webp",
+            ]),
+            ([
+                "data_id"   => 5,
+                "name"   => "Galeri hotel swiss",
+                "picture"   => "galeri-swiss-2.webp",
+            ]),
+            ([
+                "data_id"   => 5,
+                "name"   => "Galeri hotel swiss",
+                "picture"   => "galeri-swiss-3.webp",
+            ]),
+            ([
+                "data_id"   => 6,
+                "name"   => "Galeri hotel grage",
+                "picture"   => "galeri-grage-1.webp",
+            ]),
+            ([
+                "data_id"   => 6,
+                "name"   => "Galeri hotel grage",
+                "picture"   => "galeri-grage-2.webp",
+            ]),
+            ([
+                "data_id"   => 6,
+                "name"   => "Galeri hotel grage",
+                "picture"   => "galeri-grage-3.webp",
             ]),
         ]);
 
