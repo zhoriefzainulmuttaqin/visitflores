ba74facda8ed4ea5d02e1f8841fb6274d6be3939
Muhammad Hafiizh <muhammad.hafiizh.mail@gmail.com>
2023-11-08 05:12:17 +0700

    revisi halaman layanan produk

--- resources/views/user/product_services.blade.php
+++ resources/views/user/product_services.blade.php
@@ -66,29 +66,31 @@ use Illuminate\Support\Facades\App;
     <h1 class="text-center mb-5">
         <b>Tourism Card</b>
     </h1>
-    <div class="container mt-5">
-        <div class="card shadow" id="BuyTourismCard">
-            <div class="card-body p-5">
-                <div class="row justify-content-center">
-                    <div class="col-md-12 col-lg-5 align-items-center">
-                        <img src="{{ url('assets/layanan-produk/mockup-tourism-card.png') }}" width="95%" class="img-fluid mb-4 mb-lg-0">
+    <div class="row mt-5">
+        <div class="col-md-12 p-3 p-md-5">
+            <div class="card shadow" id="BuyTourismCard">
+                <div class="card-body p-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 col-lg-5 align-items-center">
+                            <img src="{{ url('assets/layanan-produk/mockup-tourism-card.png') }}" width="95%" class="img-fluid mb-4 mb-lg-0">
+                        </div>
+                        <div class="col-md-12 col-lg-7 align-self-center">
+                            <p class="h3">
+                            {!! __("services.tourism_card_detail_one") !!}
+                            </p>
+                            <p class="h3">
+                                {!! __("services.tourism_card_detail_two") !!}
+        
+                            </p>
+                        </div>
                     </div>
-                    <div class="col-md-12 col-lg-7 align-self-center">
-                        <p class="h3">
-                        {!! __("services.tourism_card_detail_one") !!}
-                        </p>
-                        <p class="h3">
-                            {!! __("services.tourism_card_detail_two") !!}
-
-                        </p>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="row mt-5">
-                    <div class="col-md-12 d-grid gap-2">
-                        <a href="{{ url('layanan-produk/tourism-card') }}" class="btn btn-success btn-lg">
-                            {{ __("services.buy") }} Tourism Card
-                        </a>
+                    <div class="clearfix"></div>
+                    <div class="row mt-5">
+                        <div class="col-md-12 d-grid gap-2">
+                            <a href="{{ url('layanan-produk/tourism-card') }}" class="btn btn-success btn-lg">
+                                {{ __("services.buy") }} Tourism Card
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -102,7 +104,7 @@ use Illuminate\Support\Facades\App;
     <div class="clearfix"></div>
     <div class="row justify-content-center">
         @foreach($patterns as $pattern)
-        <div class="col-sm-12 col-md-6 p-5">
+        <div class="col-sm-12 col-md-6 p-3 p-md-5 d-flex align-items-stretch">
             <div class="card rounded shadow fixed-height">
                 <img src="{{ url('assets/patterns/'.$pattern->cover_picture) }}" class="card-img-top">
                 <div class="card-body" style="padding-left: 10%; padding-right: 10%;">
@@ -117,13 +119,11 @@ use Illuminate\Support\Facades\App;
                         {!! $pattern->details_en !!}
                     </p>
                     @endif
-                </div>
-                <div class="card-footer">
-                    <div class="d-grid gap-2">
+                    <p>
                         <a href="{{ url('paket-wisata/'.$pattern->slug) }}" class="btn btn-info bg-btn-visit text-white">
                             {{ __("services.show_details") }}
                         </a>
-                    </div>
+                    </p>
                 </div>
             </div>
         </div>
@@ -136,7 +136,7 @@ use Illuminate\Support\Facades\App;
     <div class="clearfix"></div>
     <div class="row justify-content-center">
         @foreach($gifts as $gift)
-        <div class="col-sm-12 col-md-6 p-5">
+        <div class="col-sm-12 col-md-6 p-3 p-md-4 mb-3 mb-md-0 d-flex align-items-stretch">
             <div class="card rounded shadow">
                 <div class="card-body" style="padding-left: 10%; padding-right: 10%;">
                     <img src="{{ url('assets/layanan-produk/'.$gift->picture) }}" class="img-fluid rounded" width="100%">
